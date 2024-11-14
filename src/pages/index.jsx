64ea6881914c0d87c5dbe5082af862b7e259ee98
Arcee69 <olawalearcee@gmail.com>
2024-11-14 19:46:01 +0100

    Hot Fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import BottleA from '../assets/pdf/Bottle Price List-1.png';
 import BottleB from '../assets/pdf/Bottle Price List-2.png';
 import BottleC from '../assets/pdf/Bottle Price List-3.png';
-import Cocktail from '../assets/pdf/Cocktail Menu-1.png';
+import CocktailA from '../assets/pdf/Cocktail Menu-1.png';
+import CocktailB from '../assets/pdf/Cocktail Menu-2.png';
 import Mocktail from '../assets/pdf/Mocktail Menu-1.png';
 import Shots from '../assets/pdf/Shots & Shooters-1.png';
 import FoodA from '../assets/pdf/Food Price List-1.png';
@@ -12,7 +13,7 @@ import FoodC from '../assets/pdf/Food Price List-3.png';
 import FoodD from '../assets/pdf/Food Price List-4.png';
 import Grills from '../assets/pdf/GRILLS PRICE LIST-1.png';
 import Beer from '../assets/pdf/BEER MENU-1.png';
-import Shisha from '../assets/pdf/Shisha-1.png';
+import Shisha from '../assets/pdf/Cigarette & Shisha-1.png';
 
 
 const HomePage = () => {
@@ -25,7 +26,8 @@ const HomePage = () => {
         <img src={FoodD} alt="Menu" style={{ width: '100%' }} />
         <img src={Grills} alt="Menu" style={{ width: '100%' }} />
         <img src={Mocktail} alt="Menu" style={{ width: '100%' }} />
-        <img src={Cocktail} alt="Menu" style={{ width: '100%' }} />
+        <img src={CocktailA} alt="Menu" style={{ width: '100%' }} />
+        <img src={CocktailB} alt="Menu" style={{ width: '100%' }} />
         <img src={Shots} alt="Menu" style={{ width: '100%' }} />
         <img src={BottleA} alt="Menu" style={{ width: '100%' }} />
         <img src={BottleB} alt="Menu" style={{ width: '100%' }} />
